05a44940a8dd5806b314ff7b65eb0c361687810c
Gavin Clarke <ggaavviinnccllaarrkkee@hotmail.com>
2011-04-09 12:13:57 +0100

    fixed merge

--- v2/src/Motherboard/boards/rrmbv12/StepperInterface.cc
+++ v2/src/Motherboard/boards/rrmbv12/StepperInterface.cc
@@ -17,7 +17,6 @@
 
 #include "StepperInterface.hh"
 #include "EepromMap.hh"
-#include "Configuration.hh"
 
 void StepperInterface::setDirection(bool forward) {
 	if (invert_axis) forward = !forward;
@@ -34,14 +33,12 @@ void StepperInterface::setEnabled(bool enabled) {
 }
 
 bool StepperInterface::isAtMaximum() {
-	if (max_pin.isNull()) return false;
 	bool v = max_pin.getValue();
 	if (invert_endstops) v = !v;
 	return v;
 }
 
 bool StepperInterface::isAtMinimum() {
-	if (min_pin.isNull()) return false;
 	bool v = min_pin.getValue();
 	if (invert_endstops) v = !v;
 	return v;
@@ -52,13 +49,10 @@ void StepperInterface::init(uint8_t idx) {
 	step_pin.setDirection(true);
 	enable_pin.setValue(true);
 	enable_pin.setDirection(true);
-//	max_pin.setDirection(false);
-//	min_pin.setDirection(false);
+	max_pin.setDirection(false);
+	min_pin.setDirection(false);
 	// get inversion characteristics
 	uint8_t axes_invert = eeprom::getEeprom8(eeprom::AXIS_INVERSION, 1<<1);
-#ifdef DEFAULT_INVERSIONS
-	axes_invert ^= DEFAULT_INVERSIONS;
-#endif
 	uint8_t endstops_invert = eeprom::getEeprom8(eeprom::ENDSTOP_INVERSION, 0);
 	bool endstops_present = (endstops_invert & (1<<7)) != 0;
 	// If endstops are not present, then we consider them inverted, since they will
@@ -66,12 +60,7 @@ void StepperInterface::init(uint8_t idx) {
 	invert_endstops = !endstops_present || ((endstops_invert & (1<<idx)) != 0);
 	invert_axis = (axes_invert & (1<<idx)) != 0;
 	// pull pins up to avoid triggering when using inverted endstops
-	if (!max_pin.isNull()) {
-		max_pin.setDirection(false);
 		max_pin.setValue(invert_endstops);
-	}
-	if (!min_pin.isNull()) {
-		min_pin.setDirection(false);
 		min_pin.setValue(invert_endstops);
 	}
 }
